a0cf777fdc6c8b8c1c2224311dd1d5eab92a170a
Ander2003 <ander.delaiglesiape@elorrieta-errekamari.com>
2023-11-10 11:59:43 +0100

    Inicio

--- app/src/main/java/com/example/anderdelaiglesiaperexexameval1/MainActivity.java
+++ app/src/main/java/com/example/anderdelaiglesiaperexexameval1/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.anderdelaiglesiaperexexameval1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    ImageView IimgLogo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        IimgLogo = findViewById(R.id.IimgLogo);
+
+        IimgLogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Menu.class);
+                startActivity(intent);
+            }
+        });
     }
 }
